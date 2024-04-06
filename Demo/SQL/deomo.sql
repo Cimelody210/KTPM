f982d5bb76b890b0309395ce70a4d4bd1aa0412f
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-04-06 15:06:44 +0700

    edit

---
+++ Demo/SQL/deomo.sql
@@ -0,0 +1,36 @@
+
+create table ChuTro(
+
+    Ten varchar(50),
+    SoDienThoai float,
+    DiaChi varchar(100),
+);
+
+create table ThongTinTro(
+
+    ChuSoHuu varchar(40),
+    DiaChi varchar(60),
+    HinhAnh varchar(50),
+    GiaPhong float,
+    MoTa varchar(40),
+    
+    primary key(ChuSoHuu, DiaChi)
+);
+
+create table SinhVien(
+    HoTen varchar(50),
+    
+    -- 1: Male, 2: Female
+    GioiTinh int,
+    
+    SDT float,
+    DiaChi; varchar(50)
+);
+
+insert into ChuTro(Ten, SoDienThoai, DiaChi) values("Nguyen Van A", 28475685,"Mo ta nam o day");
+insert into ChuTro(Ten, SoDienThoai, DiaChi) values("Nguyen Van B", 28475685," in paper-books ");
+
+select * from ChuTro;
+select * from SinhVien;
+select * from ThongTinTro;
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # KTPM
+Link online:
