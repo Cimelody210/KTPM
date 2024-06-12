0a1aa9dc0e70be124c2d9d4e5aeec7922ebc4976
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-06-12 09:29:59 +0700

    Add room

--- Lab3/Hotel.cs
+++ Lab3/Hotel.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Collection.Generic;
 using NUnit.Framework;
 using YourHotelApp.Models;
 
@@ -11,7 +14,14 @@ namespace YourHotelApp.Tests
         {
             // Arrange
             var hotelManager = new HotelManager();
-            var room = new Room { Number = "101", Type = RoomType.Standard, Rate = 100 };
+            var room1 = new Room { Number = "101", Type = RoomType.Standard, Rate = 100 };
+            var room2 = new Room { Number = "102", Type = RoomType.Standard, Rate = 50 };
+            var room3 = new Room { Number = "103", Type = RoomType.Standard, Rate = 50 };
+            var room4 = new Room { Number = "104", Type = RoomType.Standard, Rate = 50 };
+            var room5 = new Room { Number = "105", Type = RoomType.Standard, Rate = 50 };
+            var room6 = new Room { Number = "106", Type = RoomType.Standard, Rate = 50 };
+            var room7 = new Room { Number = "107", Type = RoomType.Standard, Rate = 50 };
+            var room8 = new Room { Number = "108", Type = RoomType.Standard, Rate = 50 };
 
             // Act
             var result = hotelManager.AddRoom(room, UserRole.Manager);
@@ -26,6 +36,13 @@ namespace YourHotelApp.Tests
             // Arrange
             var hotelManager = new HotelManager();
             var room = new Room { Number = "101", Type = RoomType.Standard, Rate = 100 };
+            var room2 = new Room { Number = "102", Type = RoomType.Standard, Rate = 50 };
+            var room3 = new Room { Number = "103", Type = RoomType.Standard, Rate = 50 };
+            var room4 = new Room { Number = "104", Type = RoomType.Standard, Rate = 50 };
+            var room5 = new Room { Number = "105", Type = RoomType.Standard, Rate = 50 };
+            var room6 = new Room { Number = "106", Type = RoomType.Standard, Rate = 50 };
+            var room7 = new Room { Number = "107", Type = RoomType.Standard, Rate = 50 };
+            var room8 = new Room { Number = "108", Type = RoomType.Standard, Rate = 50 };
             hotelManager.AddRoom(room, UserRole.Manager);
 
             // Act
