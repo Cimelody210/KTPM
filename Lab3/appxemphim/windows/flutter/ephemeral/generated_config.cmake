8cbc35ae3af459dc43f4999ea20fb70fe2cbac0a
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-08-07 14:54:58 +0700

    Add files via upload

---
+++ Lab3/appxemphim/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter_windows_3.22.1-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Hoc Tap\\Bao Cao Cuoi Mon\\Lap Trinh Di Dong\\appxemphim" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter_windows_3.22.1-stable\\flutter"
+  "PROJECT_DIR=D:\\Hoc Tap\\Bao Cao Cuoi Mon\\Lap Trinh Di Dong\\appxemphim"
+  "FLUTTER_ROOT=C:\\dev\\flutter_windows_3.22.1-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Hoc Tap\\Bao Cao Cuoi Mon\\Lap Trinh Di Dong\\appxemphim\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Hoc Tap\\Bao Cao Cuoi Mon\\Lap Trinh Di Dong\\appxemphim"
+  "FLUTTER_TARGET=D:\\Hoc Tap\\Bao Cao Cuoi Mon\\Lap Trinh Di Dong\\appxemphim\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC81NWVhZTY4NjRiMjk2ZGQ5ZjQzYjJjYzc1NzdlYzI1NmU1YzMyYThkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Hoc Tap\\Bao Cao Cuoi Mon\\Lap Trinh Di Dong\\appxemphim\\.dart_tool\\package_config.json"
+)
